988773fc9c09bc0c7f81f7db9d57e9fd536c739c
henrique-mourao <henriquegmour4@gmail.com>
2025-03-20 17:25:03 -0300

    feat/portifolio

---
+++ assets/js/desafios.js
@@ -0,0 +1,11 @@
+function desafios(section) {
+  section.innerHTML = 
+    `<header>
+      
+
+      </div>
+      <a href="#projects" id="backToProjectsBtn" class="button">Voltar</a>
+    </div>`
+}
+
+export { desafios }
